f9b94fd0c2a4843d073eb038eacb296f64b5f2b1
rony100801 <setiawanrony603@gmail.com>
2023-04-08 20:22:12 +0800

    Membuat user interface halaman login file index

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,38 @@
   <title>Login</title>
   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
+
 <body>
 
-  <h1>Selamat Datang</h1>
+  <div class="container mt-5 pt-5">
+    <div class="row justify-content-center mt-5">
+      <div class="col-md-4 text-center">
+        <h3>Silahkan Login!</h3>
+      </div>
+    </div>
 
-<script src="assets/js/bootstrap.bundle.min.js"></script>
+    <div class="row justify-content-center mt-4">
+      <div class="col-md-4">
+        <form action="" method="post">
+
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="username" placeholder="Masukkan username..." name="username" autocomplete="off" autofocus>
+            <label for="username">Username</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="password" placeholder="Masukkan password..." name="password">
+            <label for="password">Password</label>
+          </div>
+          <div class="d-grid gap-2 mt-4">
+            <button class="btn btn-primary" type="button">Masuk</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
